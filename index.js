a8ea0722d82956d4ccff281469ddc2bce43dde90
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-02-19 10:59:35 -0800

    Expose runAdapter function

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 var _ = require('lodash');
-var TerminalAdapter = require('./node_modules/yeoman-environment/lib/adapter');
+var chalk = require('chalk');
 var Environment = require('yeoman-environment');
+var path = require('path');
+var TerminalAdapter = require('./node_modules/yeoman-environment/lib/adapter');
 
-var liferayThemeAdapter = function() {};
+var generatorLiferayTheme = path.resolve('./node_modules/generator-liferay-theme');
+
+var liferayThemeAdapter = function(data) {
+	this.data = data;
+};
 
 liferayThemeAdapter.prototype = _.merge(
 	TerminalAdapter.prototype,
 	{
 		prompt: function(prompts, callback) {
-			callback(
-				{
-					supportCompass: false,
-					themeId: 'test',
-					themeName: 'Test Theme'
-				}
-			);
+			callback(this.data);
 		}
 	}
 );
 
-var env = Environment.createEnv(null, null, new liferayThemeAdapter());
+var runGenerator = function(generator, path, data) {
+	var env = Environment.createEnv(null, null, new liferayThemeAdapter(data));
 
-env.register('./node_modules/generator-liferay-theme');
+	env.register(generatorLiferayTheme);
 
-env.run('liferay-theme');
+	try {
+		process.chdir(path);
+
+		env.run(generator);
+	}
+	catch (err) {
+		console.log(chalk.red(path + ': does not exist! Please choose a valid directory.'));
+
+		console.log(err.stack);
+	}
+};
+
+module.exports = runGenerator;
